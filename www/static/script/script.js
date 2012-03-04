f13e494d842a63587b548df80bf14d8de9b9eb15
Clint Andrew Hall <clint@clintandrewhall.com>
2012-03-04 02:41:36 -0800

    More updates

--- www/static/script/script.js
+++ www/static/script/script.js
@@ -20,6 +20,49 @@
     document.addEventListener("deviceready", onDeviceReady, false);
   }
 
+  function typeText(element, callback) {
+    var text = element.innerHTML;
+    element.style.height = element.offsetHeight + "px";
+    element.innerHTML = "";
+    element.style.visibility = "visible";
+
+    var l = text.length,
+        i = 0;
+
+    function addLetter() {
+      element.innerHTML += text[i];
+      i++;
+      if(i < l) {
+        setTimeout(addLetter, 50);
+      }
+      else {
+        if(callback) {
+          callback();
+        }
+      }
+    }
+
+    addLetter();
+  }
+
+  function loadDashboard() {
+    $("#dashboard .stats").bind("webkitTransitionEnd", function() {
+      setTimeout(function() {
+        $("#dashboard .dialog.wizard").bind("webkitAnimationEnd", function() {
+          typeText($("#dashboard .dialog.wizard .typeable")[0]);
+        }).addClass("show");
+      }, 500);
+    }).addClass("show");
+  }
+
+  function loadTown() {
+    $("#welcome").bind("webkitTransitionEnd", function() {
+      loadDashboard();
+    });
+    $("#welcome").addClass("hide");
+    return false;
+  }
+
   /* When this function is called, PhoneGap has been initialized and is ready to roll */
   /* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
   see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
@@ -37,6 +80,16 @@
         page = $(".page"),
         vol = $(".sound a"),
         sound = true;
+
+    page.bind("webkitTransitionEnd", function() {
+      $("#welcome .wizard").bind("webkitAnimationEnd", function() {
+        typeText($("#welcome .dialog .typeable")[0], function() {
+          $("#welcome .choice").addClass("show");
+        });
+      }).addClass("show");
+    });
+
+    $("#welcome .choice").bind("tap", loadTown);
 
     intro.play();
 
@@ -58,9 +111,25 @@
           tinkle.play();
           setTimeout(function() {
             theme.play();
+            coin.release();
+            tinkle.release();
           }, 3000)
         }, 1000);
       }, 100);
       return false;
     });
   }
+
+  // Enable pusher logging - don't include this in production
+  Pusher.log = function(message) {
+    if (window.console && window.console.log) window.console.log(message);
+  };
+
+  // Flash fallback logging - don't include this in production
+  WEB_SOCKET_DEBUG = true;
+
+  var pusher = new Pusher('8159432ab8d315714b65');
+  var channel = pusher.subscribe('test_channel');
+  channel.bind('my_event', function(data) {
+    alert(data);
+  });
